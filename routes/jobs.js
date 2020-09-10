be80886683da533a3422c3c575c91e427226f83f
masayado <masayado@uc.cl>
2020-09-10 20:10:21 -0300

    re edite estilo del formulario y está terminado

--- routes/jobs.js
+++ routes/jobs.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET jobs page. */
+/* GET jobs listing. */
 router.get('/', function(req, res, next) {
   res.render('jobs', { title: 'Express' });
 });
