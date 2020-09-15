0f5bfa5f3bb159ae631e68b9b07ab5bc196e348d
masayado <masayado@uc.cl>
2020-09-15 03:27:20 -0300

    edite estilos y hice funcion printJob para imprimir lista de trabajos en la vista

--- public/actions/jobs.js
+++ public/actions/jobs.js
@@ -1,47 +1,56 @@
 const BASE_URL = "https://cors-anywhere.herokuapp.com/https://jobs.github.com/positions.json?"
 
+$(function (){
+    getJobs("python","usa"); //esta trayendo la data
+});
+
 //evento button
 $(function () {
-    $("#buscar").click(function (e) {
-    getJobs();
+    $("#buscar_empleo").click(function (e) {
       e.preventDefault();
       var pais = $("#pais").find(":selected").text();
       var afinidad= $("#trabajo").find(":selected").text();
-      console.log(pais, afinidad) //funciona
+      console.log(pais, afinidad) //funciona!
     });   
 });
 
-function getJobs(type,location){
+function getJobs(type, location){
     axios.get(`${BASE_URL}description=${type}&location=${location}`)
-        .then((response)=>{
-            //
-        console.log(response);
-            response.data.forEach((job)=>{
-                printJob(job);
-            })
-            console.log(response);
-        })
-        .catch((error)=>{
-            console.log(error);
-        })
-} 
+    .then((resp)=>{
+        //
+    resp.data.forEach((job)=>{
+        printJob(job);
+    })
+    console.log(resp);
+    })
+    .catch((error)=>{
+        console.log(error);
+    })
+}
 
-function printJob(){
-    var content =`
-    <div id="jobs-container" class="jobs-container">
-        <div class="location">
-        ${job.location}
+function printJob(job){
+    var content = `
+    <div class="card mb-3">
+    <h5 class="card-header">${job.title}</h5>
+      <div class="card-body">
+        <div class="row">
+        <div class="col md-4">
+        <p class="font-weight-bold">Company</p>
+        <p class>${job.company}</p>
+      </div>
+          <div class="col md-4">
+            <p class="font-weight-bold">Location</p>
+            <p class>${job.location}</p>
+          </div>
+          <div class="col md-4">
+            <p class="font-weight-bold">Type</p>
+            <p>${job.type}</p>
+          </div>
         </div>
-        <div class="company">
-        ${job.company}
-        </div>
-        <div class="type">
-        ${job.type}
-        </div>
-        <div class="description">
-        ${job.description}
-        </div>
-    </div>
-    `
-    $('#jobs-container').append(content);
+        <hr>
+        <p class="card-text">${job.description}</p>
+      </div>
+  </div>
+     `
+        $('#jobs_container').append(content);
 }
